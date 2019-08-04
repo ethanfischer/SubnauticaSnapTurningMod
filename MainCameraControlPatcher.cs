2853f33851981f7c850c71088dc35dd729f08405
EthanFischer <ethan.f.fischer@gmail.com>
2019-08-04 17:43:33 -0500

    This works

---
+++ MainCameraControlPatcher.cs
@@ -0,0 +1,38 @@
+﻿using Harmony;
+using UnityEngine;
+using UnityEngine.VR;
+
+namespace SubnauticaSnapTurningMod
+{
+    [HarmonyPatch(typeof(MainCameraControl))]
+    [HarmonyPatch("Update")]
+    internal class MainCameraControlPatcher
+    {
+        const float SNAP_AMOUNT = 45f;
+
+        [HarmonyPrefix]
+        public static bool Prefix()
+        {
+            bool shouldSnapTurn = Player.main.motorMode != Player.MotorMode.Vehicle && VRSettings.enabled;
+            if (!shouldSnapTurn)
+            {
+                return true; //Enter vanilla method as usual
+            }
+
+            var newEulerAngles = Player.main.transform.localRotation.eulerAngles;
+
+            if (GameInput.GetButtonDown(GameInput.Button.LookRight))
+            {
+                newEulerAngles.y += SNAP_AMOUNT;
+            }
+            else if (GameInput.GetButtonDown(GameInput.Button.LookLeft))
+            {
+                newEulerAngles.y -= SNAP_AMOUNT;
+            }
+
+            Player.main.transform.localRotation = Quaternion.Euler(newEulerAngles);
+
+            return false; //Don't enter vanilla method
+        }
+    }
+}
---
+++ MainPatcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Harmony;
+
+namespace SubnauticaSnapTurningMod
+{
+    public class MainPatcher
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.oldark.subnautica.acceleratedstart.mod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
