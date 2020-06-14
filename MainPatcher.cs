e2ee7a7d5b6cbed63b52869cd76159f61a5daf78
EthanFischer <ethan.f.fischer@gmail.com>
2020-06-14 18:50:04 -0500

    Use QMod attributes for mod entry point

--- MainPatcher.cs
+++ MainPatcher.cs
@@ -1,11 +1,14 @@
 ﻿using System.Reflection;
-using Harmony;
 using SMLHelper.V2.Handlers;
+using QModManager.API.ModLoading;
+using Harmony;
 
 namespace SubnauticaSnapTurningMod
 {
-    public class MainPatcher
+    [QModCore]
+    public static class MainPatcher
     {
+        [QModPatch]
         public static void Patch()
         {
             Config.Load();
